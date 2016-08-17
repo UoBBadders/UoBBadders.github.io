93f640a44184ce0bf309b269363911706d9644d4
Michael Christensen <Michael Christensen>
2016-08-17 14:00:16 +0100

    nav bar clicks scroll to sections, added real data and photos for the team

--- sections/header/header.controller.js
+++ sections/header/header.controller.js
@@ -3,7 +3,14 @@ app.controller('headerController', function($scope) {
   // TODO: animate header.
   // Strategy: instead of switching image urls, fade top image to 0 opacity, then set bottom image as top image (z-index).
   // Should be down in directive.
-  $scope.images = ['assets/images/cover1.jpg', 'assets/images/cover2.jpg'];
+  $scope.images = ['assets/images/cover1.jpg',
+                   'assets/images/cover2.jpg',
+                   'assets/images/cover3.jpg',
+                   'assets/images/cover4.jpg',
+                   'assets/images/cover5.jpg',
+                   'assets/images/cover6.jpg',
+                   'assets/images/cover7.jpg',
+                   'assets/images/cover8.jpg'];
   $scope.idx = 0;
 
   function mod(x, m) {
