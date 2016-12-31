b2a5a248ce0c240b20ef8d432d214d4a407f73d7
UoBBadders <uobbadders@gmail.com>
2016-12-31 16:54:58 +0000

    Update editable.js

--- EDITME/editable.js
+++ EDITME/editable.js
@@ -161,7 +161,7 @@ app.constant('captains', [
     position: 'Men\'s 2nd Captain',
     email: 'bm157310@my.bristol.ac.uk'
   }, {
-    image: 'Georgia Alt.png',
+    image: 'Georgia Higgs.png',
     name: 'Georgia Higgs',
     position: 'Ladies\' 1st Captain',
     email: 'gh15476@my.bristol.ac.uk'
