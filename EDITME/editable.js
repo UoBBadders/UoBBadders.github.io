e60e211ebdac47f31906cf14be8eeeec990f97f1
UoBBC <uobbadders@outlook.com>
2023-08-22 14:19:45 +0100

    Update editable.js
    Added Kemps Jewellers

--- EDITME/editable.js
+++ EDITME/editable.js
@@ -72,6 +72,9 @@ app.constant('supports', [
   {
     link: 'https://www.bristolsu.org.uk/',
     image: 'Supported_By_Bristol_SU_Logo_Blue.png'
+  }  {
+    link: 'https://www.kempsjewellers.com/',
+    image: 'KempsLogoBlack.png'
   }
   // ---- END EDIT HERE ----
 ]);
